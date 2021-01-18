4486822bad21112e96db4ace00fcaf9fa2be0749
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-01-18 23:22:42 +0200

    all

---
+++ src/moduls/test-moduls.js
@@ -0,0 +1,26 @@
+const secretNumber = 10;
+
+const setting = {
+  lang: 'fi'
+};
+
+
+const sayHello = (toWho) => {
+  return 'Moro' + toWho;
+};
+
+//export default sayHello;
+
+/*export {
+  sayHello,
+  secretNumber,
+  setting
+};*/
+
+/*myModule = {
+  sayHello: sayHello,
+  secretNumber: secretNumber,
+  setting: setting
+};
+
+export default myModule;*/
